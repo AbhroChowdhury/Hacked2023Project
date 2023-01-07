6b9e74e2c661a0d24be76e83471297088cea805d
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-07 15:11:22 -0700

    update login window

--- loginfeature.py
+++ loginfeature.py
@@ -7,11 +7,17 @@ from tkinter import *
 # creating window with tkinter
 root = Tk()
 
-# adjusting size and window title, along with background image 
+# adjusting size and window title, adding in background image 
 root.geometry('1000x600+100+100')
 root.title('Login Page')
-LoginBackground = PhotoImage(file='background1x.png')
+LoginBackground = PhotoImage(file='background6x.png')
 LoginBackgroundLabel =  Label(root, image=LoginBackground)
 LoginBackgroundLabel.place(x=0, y=0)
+
+# Styling Text Box that will include all the login features
+LoginFrame = Frame(root, width = 600, height = 300, bg='white')
+LoginFrame.place(x=200, y=140)
+
+
 
 root.mainloop()
