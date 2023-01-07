618f072902fce0249abfd52b6d1f1c038e7c8c20
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-07 12:13:32 -0700

    setting up API

--- receiptscanner.py
+++ receiptscanner.py
@@ -1,3 +1,17 @@
 '''
 Receipt Scanner Functionality using Asprise OCR API
 '''
+
+import requests as rq    # pip3 install requests
+import json
+import pickle
+
+url = "https://ocr.asprise.com/api/v1/receipt"
+receipts = "figure1.jpg"
+
+res = rq.post(url, 
+                data = {'api_key': 'TEST', 'recognizer': 'auto', 'ref_no': 'oct_python_123'},
+                files = {'file': open(receipts, 'rb')})
+
+with open("output1.json", "w") as f:
+    json.dump(json.loads(res.text), f)
