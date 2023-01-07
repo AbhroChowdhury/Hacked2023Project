534d71abc780a75cf7cb26a4123407e93ac2e07f
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-01-07 14:17:41 -0700

    tkinter window

--- loginfeature.py
+++ loginfeature.py
@@ -4,7 +4,11 @@ Going to handle all user og in functionality here
 
 from tkinter import *
 
+# creating window with tkinter
 root = Tk()
 
+# adjusting size and window title
+root.geometry('1000x600+100+100')
+root.title('Login Page')
 
 root.mainloop()
